b994b4f0648bbb633e57743717533b5ecf1a0a87
Justin Hugon <jhugon@users.noreply.github.com>
2022-01-29 20:31:31 -0700

    ran black on neorv32.py

--- asciiserialcom/neorv32.py
+++ asciiserialcom/neorv32.py
@@ -4,14 +4,16 @@ from asciiserialcom.tty_utils import setup_tty
 
 BAUD = 19200
 
+
 async def print_fin(fin):
     while True:
         data = await fin.read()
-        text = data.decode("ASCII",errors="replace")
+        text = data.decode("ASCII", errors="replace")
         if len(text) > 0:
-            print(text,end="",flush=True)
+            print(text, end="", flush=True)
 
-async def run(serial,firmware):
+
+async def run(serial, firmware):
     timeout = 10
     setup_tty(serial, BAUD, arduino_dont_hup=False)
     with trio.move_on_after(timeout) as cancel_scope:
@@ -19,7 +21,7 @@ async def run(serial,firmware):
             async with await trio.open_file(serial, "br") as fin:
                 async with await trio.open_file(serial, "bw") as fout:
                     async with await trio.open_file(firmware, "br") as fw_file:
-                        nursery.start_soon(print_fin,fin)
+                        nursery.start_soon(print_fin, fin)
                         await trio.sleep(1)
                         await fout.write(b"u")
                         await trio.sleep(0.5)
@@ -32,22 +34,21 @@ async def run(serial,firmware):
                         await trio.sleep(2)
                         nursery.cancel_scope.cancel()
 
+
 def main():
 
     parser = argparse.ArgumentParser(
         description="Uploads firmware to NeoRV32 cores over UART. The user should run this right after resetting the core."
     )
     parser.add_argument(
-        "tty",
-        help="Location of tty to use, e.g. /dev/ttyUSB0",
+        "tty", help="Location of tty to use, e.g. /dev/ttyUSB0",
     )
     parser.add_argument(
-        "firmware",
-        help="Location of firmware file to upload",
+        "firmware", help="Location of firmware file to upload",
     )
     args = parser.parse_args()
 
     serial_location = args.tty
     firmware = args.firmware
 
-    trio.run(run,serial_location,firmware)
+    trio.run(run, serial_location, firmware)
